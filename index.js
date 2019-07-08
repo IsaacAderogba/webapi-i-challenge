bb19a273255374cc179d6a35b81b7c36e17d8307
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-07-08 12:11:36 +0100

    Configure server to parse JSON and to listen on port 3000

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-// 1 - Import express, create express app and import db methods
+// 1 - Import express, create express server and import db methods
 const express = require("express");
 const server = express();
 const User = require('./data/db')
+
+// 2 - Configure the newly created server to parse JSON
+server.use(express.json());
+
+// 3 - Create endpoints
+
+// 4 - Tell server to listen for incoming requests
+server.listen(3000, () => {
+    console.log('listening on 3000');
+});
