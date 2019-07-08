8cd033a5076d62d7b305ba666184eeabb1bd6784
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-07-08 12:35:37 +0100

    Configure post user endpoint, accounting for success and failure

--- index.js
+++ index.js
@@ -1,33 +1,52 @@
 // 1 - Import express, create express server and import db methods
 const express = require("express");
 const server = express();
-const User = require('./data/db')
+const User = require("./data/db");
 
 // 2 - Configure the newly created server to parse JSON
 server.use(express.json());
 
 // 3 - Create endpoints
-server.get('/api/users', (req, res) => {
-    res.json('get users endpoints')
+server.get("/api/users", (req, res) => {
+  res.json("get users endpoints");
 });
 
-server.get('/api/users/:id', (req, res) => {
-    res.json('get users by id endpoint')
-})
+server.get("/api/users/:id", (req, res) => {
+  res.json("get users by id endpoint");
+});
 
-server.post('/api/users/', (req, res) => {
-    res.json('post to users')
-})
+server.post("/api/users/", (req, res) => {
+  const { name, bio } = req.body;
+  if (name && bio) {
+    const user = { name, bio };
+    User.insert(user)
+      .then(data => {
+        res.status(201).json(data);
+      })
+      .catch(() => {
+        res
+          .status(500)
+          .json({
+            errorMessage:
+              "There was an error while saving the user to the database"
+          });
+      });
+  } else {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user" });
+  }
+});
 
-server.delete('/api/users/:id', (req, res) => {
-    res.json('delete user by Id')
-})
+server.delete("/api/users/:id", (req, res) => {
+  res.json("delete user by Id");
+});
 
-server.put('/api/users/:id', (req, res) => {
-    res.json('put user by Id')
-})
+server.put("/api/users/:id", (req, res) => {
+  res.json("put user by Id");
+});
 
 // 4 - Tell server to listen for incoming requests
 server.listen(3000, () => {
-    console.log('listening on 3000');
+  console.log("listening on 3000");
 });
