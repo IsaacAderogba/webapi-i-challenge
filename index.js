abd0337c1c919721e8384b9a0ae109acb3f51c59
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-07-08 12:19:43 +0100

    Configure and test basic endpoints with postman

--- index.js
+++ index.js
@@ -7,6 +7,25 @@ const User = require('./data/db')
 server.use(express.json());
 
 // 3 - Create endpoints
+server.get('/api/users', (req, res) => {
+    res.json('get users endpoints')
+});
+
+server.get('/api/users/:id', (req, res) => {
+    res.json('get users by id endpoint')
+})
+
+server.post('/api/users/', (req, res) => {
+    res.json('post to users')
+})
+
+server.delete('/api/users/:id', (req, res) => {
+    res.json('delete user by Id')
+})
+
+server.put('/api/users/:id', (req, res) => {
+    res.json('put user by Id')
+})
 
 // 4 - Tell server to listen for incoming requests
 server.listen(3000, () => {
